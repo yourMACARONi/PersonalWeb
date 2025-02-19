c402374f31fcabfe4828de41f85de184bab010f3
yourMACARONi <craususrhyle@gmail.com>
2025-02-18 16:34:23 -1100

    first commit

---
+++ personalweb/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <h2 className="text-2xl font-sans">I'm Charlie Quipanes</h2>
+      <h3 className="text-xl font-serif">
+        from University of Science and Technology of southern Philippines
+      </h3>
+    </main>
+  );
+}
